9a440ec22e215fb127c7203516a6cf8628fe0c67
Victor Wang <71830568+ZKBig@users.noreply.github.com>
2025-09-01 12:37:28 -0500

    Add files via upload

---
+++ sft.py
@@ -0,0 +1,171 @@
+import os
+from dataclasses import dataclass, field, asdict
+from typing import Optional
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+from datasets import load_dataset, concatenate_datasets, DatasetDict
+import transformers
+from transformers import PreTrainedTokenizer, PreTrainedModel
+import trl
+import torch
+from peft import LoraConfig
+import math
+
+def tokenizer_and_embedding_resize(
+    special_tokens_dict: dict,
+    tokenizer: PreTrainedTokenizer,
+    model: PreTrainedModel,
+) -> dict:
+    """Resize tokenizer and embedding.
+    Args:
+        special_tokens_dict: Dict containing special tokens to be added.
+        tokenizer: transformers.PreTrainedTokenizer.
+        model: transformers.PreTrainedModel
+    Return:
+        dict: Metadata on number of added tokens
+    """
+    num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+    embedding_size = math.ceil(len(tokenizer))
+    num_new_tokens = num_new_tokens + embedding_size - len(tokenizer)
+    model.resize_token_embeddings(embedding_size)
+    if num_new_tokens > 0:
+        input_embeddings = model.get_input_embeddings().weight.data
+        output_embeddings = model.get_output_embeddings().weight.data
+
+        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(
+            dim=0, keepdim=True
+        )
+        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(
+            dim=0, keepdim=True
+        )
+
+        input_embeddings[-num_new_tokens:] = input_embeddings_avg
+        output_embeddings[-num_new_tokens:] = output_embeddings_avg
+
+    
+
+def formatting_prompts_func(example):
+    output_texts = []
+    for i in range(len(example['instruction'])):
+        text = f"### Question: {example['instruction'][i]}\n ### Answer: {example['output'][i]}"
+        output_texts.append(text)
+    return output_texts
+
+@dataclass
+class TrainingConfig:
+    model_name: str = field(default="nvidia/NVIDIA-Nemotron-Nano-9B-v2")
+    block_size: int = field(default=32768)
+    wandb_project: Optional[str] = field(default="s1")
+    wandb_entity: Optional[str] = field(default="hashimoto-group")
+    train_file_path: Optional[str] = field(default='simplescaling/s1K_tokenized')
+    dagger: bool = field(default=False)
+
+    def __post_init__(self):
+        os.environ['WANDB_PROJECT'] = self.wandb_project
+
+def train():
+    # parsing input
+    parser = transformers.HfArgumentParser((TrainingConfig, trl.SFTConfig))
+    config, args = parser.parse_args_into_dataclasses()
+    log_config = {**asdict(config), **asdict(args)}
+    logging.info(f"Training config: {log_config}")
+
+    # loading model
+    kwargs = {}
+    if "70B" in config.model_name:
+        # Removed "low_cpu_mem_usage": True, for 70B, since by default we are in FSDP,
+        # it's more efficient to do  "cpu_ram_efficient_loading": true, in fsdp_config.json
+        kwargs = {"device_map": "auto", "torch_dtype": "auto",
+                  "attn_implementation": "flash_attention_2", "use_cache": False}
+        model = transformers.AutoModelForCausalLM.from_pretrained(config.model_name, **kwargs)
+    else:
+        model = transformers.AutoModelForCausalLM.from_pretrained(config.model_name, trust_remote_code=True, use_cache=False)
+
+    dataset = load_dataset(config.train_file_path)
+
+    # setting up trainer
+    tokenizer = transformers.AutoTokenizer.from_pretrained(config.model_name, trust_remote_code=True, use_fast=True)
+    if "Llama" in config.model_name:
+        instruction_template = "<|start_header_id|>user<|end_header_id|>"
+        response_template = "<|start_header_id|>assistant<|end_header_id|>\n\n"
+        # Use a token that is never used
+        tokenizer.pad_token = "<|reserved_special_token_5|>"
+    elif "Qwen" in config.model_name:
+        instruction_template = "<|im_start|>user"
+        response_template = "<|im_start|>assistant\n"
+        # Use a token that is never used
+        tokenizer.pad_token = "<|fim_pad|>"
+    elif "NVIDIA" in config.model_name:
+        instruction_template = "<|im_start|>user"
+        response_template = "<|im_start|>assistant\n"
+        # Use a token that is never used
+        tokenizer.pad_token = "<|fim_pad|>"
+
+    if "bamba" in config.model_name:
+        special_tokens_dict = {}
+        # if tokenizer.pad_token is None:
+        print("PAD token set to <PAD>, missing in tokenizer")
+        special_tokens_dict["pad_token"] = "<PAD>"
+
+        # tokenizer_and_embedding_resize(
+        #     special_tokens_dict=special_tokens_dict,
+        #     tokenizer=tokenizer,
+        #     model=model,
+        # )
+        num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+        embedding_size = math.ceil(len(tokenizer))
+        num_new_tokens = num_new_tokens + embedding_size - len(tokenizer)
+        model.resize_token_embeddings(embedding_size)
+        if num_new_tokens > 0:
+            input_embeddings = model.get_input_embeddings().weight.data
+            output_embeddings = model.get_output_embeddings().weight.data
+
+            input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(
+                dim=0, keepdim=True
+            )
+            output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(
+                dim=0, keepdim=True
+            )
+
+            input_embeddings[-num_new_tokens:] = input_embeddings_avg
+            output_embeddings[-num_new_tokens:] = output_embeddings_avg
+
+    # Only compute loss over assistant responses
+    # Verified that it precisely starts where the thinking tokens start and ends with the first pad token
+    # via labels being set to -100
+    collator = trl.DataCollatorForCompletionOnlyLM(
+        instruction_template=instruction_template,
+        response_template=response_template,
+        tokenizer=tokenizer,
+        mlm=False
+    )
+    args.dataset_text_field = 'text'
+    args.max_seq_length = config.block_size
+    args.per_device_train_batch_size = 1
+
+    peft_config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+
+    trainer = trl.SFTTrainer(
+        model,
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['test'] if 'test' in dataset else dataset['train'],
+        args=args,
+        data_collator=collator
+    )
+
+    trainer.train()
+    trainer.save_model(output_dir=args.output_dir)
+    tokenizer.save_pretrained(args.output_dir)
+    trainer.accelerator.wait_for_everyone()
+
+
+if __name__ == "__main__":
+    train()
